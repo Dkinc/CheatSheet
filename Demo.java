1f8709348bae04248fad8cc5ae922800da36945c
dkinc <dkinc@abv.bg>
2016-09-07 17:06:27 +0300

    added json

--- Demo.java
+++ Demo.java
@@ -1,7 +1,12 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import com.google.gson.Gson;
 
 public class Demo {
 	
@@ -17,6 +22,30 @@ public class Demo {
 			}
 		} catch (SQLException e) {
 			System.out.println("Printing Errorr....");
+			e.printStackTrace();
+		}
+		
+		// JASON FILES
+		//IMPORT GSON
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(new File("hah") );
+		File partsJson = new File("parts.JSON");
+		if(!partsJson.exists()){
+			try {
+				partsJson.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println(json);
+		try {
+			FileWriter writer = new FileWriter(partsJson);
+			writer.write(json);
+			System.out.println("FILE CREATED");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
